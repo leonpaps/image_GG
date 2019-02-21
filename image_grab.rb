e38f13bb401a4593411036c59b9a38c94458db6c
leon <leonpaps@gmail.com>
2019-02-21 16:59:18 +0200

    adds saving of base64 images

--- image_grab.rb
+++ image_grab.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'mimemagic'
 
-
+# remove danger chars
 search_term = ARGV[0] || 'hotdog'
 
 browser = Watir::Browser.new(:chrome)
@@ -16,9 +16,18 @@ browser.send_keys :enter
 
 doc = Nokogiri.parse(browser.html)
 
-image_urls = (doc.css 'img.rg_ic').map{|img|  
-  img.values.select{|v| /^http/ === v}.first 
+image_base_64 = []
+
+image_urls = (doc.css 'img.rg_ic').map{|img|
+  link = img.values.select{|v| /^http/ === v}.first 
+
+  unless link
+    image_base_64 << img.values.select{|v| /^data:image.*?base64/ === v}.first
+  end
+
+  link
 }.compact
+
 
 `mkdir "./images/#{search_term}/" `
 image_urls.each_with_index do |url, index|
@@ -26,3 +35,11 @@ image_urls.each_with_index do |url, index|
   type = MimeMagic.by_magic(download).subtype
   IO.copy_stream(download, "./images/#{search_term}/image_#{index}.#{type}")
 end
+
+image_base_64.each.with_index(image_urls.length) do |text, index|
+  match = /data:image\/(?<type>.*?);base64,(?<data>.*)/.match(text)
+  File.open("./images/#{search_term}/image_#{index}.#{match[:type]}", 'wb') do |f|
+    f.write(  Base64.decode64(match[:data]))
+  end
+end
+
